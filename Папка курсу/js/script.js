35119eec68d6f4e38f0933d61b8273124083e671
Alex <sylkoalex@gmail.com>
2022-11-09 22:08:02 +0200

    1488firstnormalcommit

--- Папка курсу/js/script.js
+++ Папка курсу/js/script.js
@@ -1,12 +1,19 @@
 "use strict";
 
-let bebri = prompt("your name?","");
-alert(`hello, ${bebri}`);
+let numberOfFilms = +prompt("Скільки фільмів ти вже подивився","");
+let personalMovieDV = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    geners:[],
+    private: false
+};
+let a = prompt("Один з останніх фільмів:","");
+let b = +prompt("Яку оцінку дасиш йому?","");
+let c = prompt("Один з останніх фільмів:","");
+let d = +prompt("Яку оцінку дасиш йому?","");
 
+personalMovieDV.movies[a] = b;
+personalMovieDV.movies[c] = d;
 
-let isOpen = false,
-    isWork = true;
-console.log(228 === '228');
-let bugaga = 1488288;
-
-const ffffffff = 222222;
+console.log(personalMovieDV);
