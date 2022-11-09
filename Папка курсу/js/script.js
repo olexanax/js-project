fdd66303bc99eb40c14a312ef92b87e5c8d06845
Alex <sylkoalex@gmail.com>
2022-11-09 19:22:43 +0200

    firstcommit

---
+++ Папка курсу/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let bebri = prompt("your name?","");
+alert(`hello, ${bebri}`);
+
+
+let isOpen = false,
+    isWork = true;
+console.log(228 === '228');
+let bugaga = 1488288;
+
+const ffffffff = 222222;
